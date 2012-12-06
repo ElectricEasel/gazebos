c07cafd46a34b1c067b4f3fb27516fa592688630
Don Gilbert <don@dongilbert.net>
2012-12-05 22:52:43 -0600

    initial commit of gazebos template

---
+++ templates/gazebos/html/com_content/featured/homepage.php
@@ -0,0 +1,15 @@
+<?php defined('_JEXEC') or die; ?>
+<div class="row">
+	<div class="span3">
+		span 3
+	</div>
+	<div class="span3">
+		span 3
+	</div>
+	<div class="span3">
+		span 3
+	</div>
+	<div class="span3">
+		span 3
+	</div>
+</div>
