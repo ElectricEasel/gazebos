530064f00c59db7bec21318610649672c6719ab6
Don Gilbert <don@electriceasel.com>
2013-05-31 08:03:45 -0500

    Adding email view to component. Fixing in .gitignore

---
+++ components/com_gazebos/views/email/tmpl/reply.php
@@ -0,0 +1,74 @@
+<?php defined('_JEXEC') or die;
+
+// Make sure the data is in object form (stdClass)
+$this->data = (object) $this->data;
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Gazebos.com Form</title>
+</head>
+
+<body>
+
+	<table background="http://gazebos.com/email/email-bg.png" style="border:1px solid #C6C1B8" cellpadding="0" cellspacing="0" border="0" width="620">
+    	<tr>
+        	<td>
+            <center>
+ 		   	<table cellpadding="0" cellspacing="0" border="0" width="600">
+            	<tr>
+                	<td colspan="2" height="60"></td>
+                </tr>
+ 		       	<tr>
+ 		       	  <td width="336">
+						<img src="http://gazebos.com/email/logo.png" />
+                  </td>
+    	        	<td width="264" valign="top" align="right">
+    	            <span style="font-size:18px;color:#5e4436;text-transform:uppercase;font-family:Arial, Helvetica, sans-serif;font-style:italic;margin-top:10px;">Phone: 1-888-4-gazebo</span>
+   	               </td>
+          	  </tr>
+              <tr>
+              	<td colspan="2" align="center">
+                	<img width="443" height="105" style="padding-top:20px;margin-bottom:10px;display:inline;text-align:center;" src="http://gazebos.com/email/thank-you.png" />
+                </td>
+              </tr>
+              <tr>
+                <td colspan="2" align="center"><p style="color:#3c2312;line-height:1.4em;font-family:Arial, Helvetica, sans-serif;font-style:italic;font-weight:bold;font-size:18px">We have received your information, and a Gazebos.com customer service representative will be in touch with you shortly.</p></td>
+              </tr>
+              <tr>
+                <td height="262" colspan="2" align="center" valign="top">
+                	<table cellpadding="0" cellspacing="0" border="0" width="550" style="margin-top:5px;">
+                    	<tr>
+                        	<td width="176" height="206">
+                            	<img src="http://gazebos.com/email/info-packet.jpg" />
+                            </td>
+                            <td width="374">
+                       	    <p style="line-height:1.4em;">Attached to this email, please find our detailed information packet. For speedier assistance, please feel free to call us at 1-888-4-GAZEBO, or email us at <a href="mailto:info@gazebos.com" style="color:#5f8824;"><span style="color:#5f8824;">info@gazebos.com</span></a>.</p>
+								<p style="line-height:1.4em;font-weight:bold;font-style:italic;color:#49352a;font-family:Arial, Helvetica, sans-serif">In the meantime, <a href="http://www.gazebos.com/blog/gazebos/choosing-the-perfect-gazebo-for-your-landscape" style="color:#5f8824;"><span style="color:#5f8824;">click here</span></a> for our Ultimate Guide to Choosing the Right Gazebo.</p>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+              </tr>
+              <tr>
+              	<td height="31" colspan="2" align="center" valign="top" style="font-family:Arial, Helvetica, sans-serif;font-size:13px;">
+                <a href="https://www.facebook.com/Gazebosdotcom" style="color:#351f11;"><span style="color:#351f11;">friend on Facebook</span></a> | <a href="https://twitter.com/gazebosdotcom" style="color:#351f11;"><span style="color:#351f11;">follow on Twitter</span></a>
+                </td>
+              </tr>
+              <tr>
+              <td height="51" colspan="2" align="center" valign="top" style="font-family:Arial, Helvetica, sans-serif;font-size:10px;">
+              <p>Copyright &copy; <?php echo date('Y');?> Gazebos.com. All rights reserved.</p>
+                </td>
+              </tr>
+              
+         </table>
+         </center>
+         </td>
+		</tr>
+	</table>
+ 	  
+
+
+</body>
+</html>
